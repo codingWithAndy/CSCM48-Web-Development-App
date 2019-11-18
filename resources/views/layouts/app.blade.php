c8f55e9bcfc00611f55331d9270cc0a18a03047f
Andy Gray <codingwithandy@gmail.com>
2019-11-18 22:58:39 +0000

    Bootstrap and CSS added to app.

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,15 +4,39 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Blogs - @yield('title')</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="/CSS/Style.css">
+    <link href="{{ asset('/CSS/Style.css') }}" rel="stylesheet">
+    <title>Bloggy Blogs - @yield('title')</title>
 </head>
 
 <body>
-        <h1>List of @yield('title')</h1>
 
     <div>
+        <nav class="navbar navbar-expand-sm bg-light navbar-light">
+            <ul class="navbar-nav">
+                <li class="nav-item active">
+                <a class="nav-link" href="#">Active</a>
+                </li>
+                <li class="nav-item">
+                <a class="nav-link" href="#">Link</a>
+                </li>
+                <li class="nav-item">
+                <a class="nav-link" href="#">Link</a>
+                </li>
+                <li class="nav-item">
+                <a class="nav-link disabled" href="#">Disabled</a>
+                </li>
+            </ul>
+        </nav>
+
+    </div>
+
+        <h1>List of @yield('title')</h1>
+
+    <div class="content">
         @yield('content')
     </div>
-    
+
 </body>
 </html>
